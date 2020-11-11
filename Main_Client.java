a2c26aca25a721a04e8fb3bb64f9bb94b89a9326
Alex <longnamesrule@gmail.com>
2020-11-11 03:08:02 -0500

    switching back to vscode

--- Main_Client.java
+++ Main_Client.java
@@ -9,12 +9,31 @@ class Main{
             System.out.println("The command line arguments are:"); 
   
             // iterating the args array and printing 
-            // the command line arguments 
+            // the command line arguments
+            //secret token
+            String secret = args[0];
+            
+            //port for server
+            try {
+	            int port = Integer.parseInt(args[1]);
+	            if(port <= 1023) {
+	            	System.out.println("you are not allowed to use a port that is between 0-1023");
+	            	break;
+	            }else if (port > 65535) {
+	            	System.out.println("you cannot use a port that doesn't exist!");
+	            	break;
+				}
+	        }catch(Exception e){
+	        	System.out.println("the port needs to be in numerical form");
+	        	break;
+	        }
+            
+            
             
         } 
         else
         {
-            System.out.println("format should be: [secret token]"); 
+            System.out.println("format should be: [secret token] [port]"); 
             break;
         }
 
--- Main_Master.java
+++ Main_Master.java
@@ -3,6 +3,9 @@ import java.io.*;
 
 class Main{
     final int NUM_ARGS = 5;
+    public ServerSocker ss;
+    public Socket s;
+    public InputStream in;
     //docker run --net=pub_net -d --ip=10.0.0.201 --hostname="test"  --name="remote-desktop" --rm --name="remote-desktop" scottyhardy/docker-remote-desktop
     public static void main(String[] args) {
         // check if length of args array is 
@@ -10,10 +13,40 @@ class Main{
         if (args.length == NUM_ARGS) 
         { 
             System.out.println("The command line arguments are:"); 
-  
+            
+            //get path
             Path path = new Path(args[0]);
+            
+            //starting ip
             String startIp = args[1];
-            int hosts =  Integer.parseInt(args[2]);
+            
+            try {
+            	int hosts =  Integer.parseInt(args[2]);
+            }catch(Exception e){
+            	System.out.println("the number of hosts needs to be in numerical form");
+            	break;
+            }
+            
+            
+            //secret token
+            String secret = args[3];
+            
+            //port for server
+            try {
+	            int port = Integer.parseInt(args[4]);
+	            if(port <= 1023) {
+	            	System.out.println("you are not allowed to use a port that is between 0-1023");
+	            	throw new Error();
+	            }else if (port > 65535) {
+	            	System.out.println("you cannot use a port that doesn't exist!");
+				}
+	        }catch(Exception e){
+	        	System.out.println("the port needs to be in numerical form");
+	        	break;
+	        }
+	        
+            ss = new ServerSocket(port);
+            
 
 
 
